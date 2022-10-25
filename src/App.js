60529a86c02563263ea6d3a0674ddfadec759a89
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2022-10-26 00:12:06 +0200

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import List from "./List.js";
 
 export default function App() {
   const [number, setNumber] = useState(1);
   const [dark, setDark] = useState(false);
 
-  const getItems = () => {
-    return [number, number + 1, number + 2];
-  };
+  const getItems = useCallback(
+    (incrementor) => {
+      return [
+        number + incrementor,
+        number + incrementor + 1,
+        number + incrementor + 2,
+      ];
+    },
+    [number]
+  );
 
   const theme = {
     backgroundColor: dark ? "#333" : "#fff",
